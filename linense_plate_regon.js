3220db93348cd2f85fc92a1f4525e6041cb297a5
Ronald Stewart <36642812+rstewart456@users.noreply.github.com>
2019-05-26 23:38:43 -0400

    Delete linense_plate_regon.js

--- linense_plate_regon.js
+++
@@ -1,8 +0,0 @@
-function recognize(s) {
-  const sample =' _  _     _  _  _  _  _ \n' +
-                ' _| _||_||_ |_   ||_||_|\n'+
-                '|_  _|  | _||_|  ||_| _|';
-  let a = sample.split();
-  console.log(a);
-  return;
-}
