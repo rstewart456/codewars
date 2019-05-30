1540aaa461806cebce25e66050372bf0bb233cb6
rstewart456 <rstewart456@gmail.com>
2019-05-30 10:28:53 -0400

    final solution

--- printererrors.js
+++ printererrors.js
@@ -1,5 +1,5 @@
 function printerError(s) {
-    var a = /[n-z]/gi ;
+    var a = /[n-z]/gi;
     var b = s.match(a);
     var c = "0"; // full lenght of s
     var d = "0"; //
@@ -11,6 +11,6 @@ function printerError(s) {
             d++;
         }
     }
-    return  d+"/"+c
+    return d + "/" + c
 }
 console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
